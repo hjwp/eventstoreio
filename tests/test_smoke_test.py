baf8bbf0dbcdfc5ba6d3bf41649f120ef93f1780
Harry <hjwp2@cantab.net>
2020-06-06 22:31:54 +0100

    trynna get ssl to work

--- tests/test_smoke_test.py
+++ tests/test_smoke_test.py
@@ -8,10 +8,17 @@ HOST = 'eventstore' if IN_CONTAINER  else 'localhost'
 HTTP_PORT = '2113' if IN_CONTAINER else '21132'
 TCP_PORT = '1113' if IN_CONTAINER else '11131'
 
+# debugging ssl errors
+import os
+os.environ["GRPC_TRACE"] = "tsi"
+os.environ["GRPC_VERBOSITY"] = "DEBUG"
+
 def test_smoke_test():
     print(f"connecting to {HOST}:{TCP_PORT}")
     credentials = grpc.ssl_channel_credentials(root_certificates=Path('server_ca.pem').read_bytes())
-    channel = grpc.secure_channel(f"{HOST}:{TCP_PORT}", credentials=credentials)
+    options = (('grpc.ssl_target_name_override', 'eventstoredb-node'),)
+    channel = grpc.secure_channel(f"{HOST}:{TCP_PORT}", credentials=credentials, options=options)
+    # grpc.channel_ready_future(channel).result()
     stub = StreamsStub(channel)
     request = ReadReq()
     response = stub.Read(request)
